520ce6eb414bce52ce0024b1c07175b9d38080d3
Aniket Mahadik <aniketmahadik212@gmail.com>
2025-06-19 15:08:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-amber-400 text-2xl font-bold text-orange-600">
+        This Is Netflix GPT 
+      </div>
+      
+    </>
+  )
+}
+
+export default App
